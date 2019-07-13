f6f50e316c9040916633da132cee54acdea396e2
Barbara Mayfield <bmayfield1991@gmail.com>
2019-07-12 19:32:57 -0500

    made new array for animals with pop less than 5, request 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -159,7 +159,8 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
+let lowerPopulation = [];
+lowerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
