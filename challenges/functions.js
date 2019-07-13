fef5f6c931f79ecfa28fcfd50df16a38bffb0aca
Barbara Mayfield <bmayfield1991@gmail.com>
2019-07-12 19:57:57 -0500

    created several functions to callback with consume();, answered closure question

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-function consume(a, b, callback) { 
-  return (a, b, callback);
+function consume(a, b, cb) { 
+  return a, b, cb;
 };
-
-consume();
-
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -20,11 +17,23 @@ consume();
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -32,6 +41,8 @@ consume();
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+
+// `nestedFunction()` can access the variable `internal` because `internal` is within the local scope.
 
 
 const external = "I'm outside the function";
